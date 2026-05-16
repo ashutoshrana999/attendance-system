46c4d9fe5f84257d548918905361bb37d8e9d9a2
Ashutosh Rana <ashutoshrana@ashutoshs-MacBook-Air.local>
2026-05-16 09:15:29 +0530

    Set login.html as default page

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,26 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    email: { type: String, unique: true },
-    password: String,
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     role: {
         type: String,
-        enum: ["user", "admin"],
+        enum: ["admin", "user"],
         default: "user"
     }
 });
